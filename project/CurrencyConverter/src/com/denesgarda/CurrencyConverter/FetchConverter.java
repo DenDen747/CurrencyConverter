ca24868f61c142d67b02d3c7c7a5866041d30eb4
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-10-06 12:40:14 -0700

    Update FetchConverter.java

--- project/CurrencyConverter/src/com/denesgarda/CurrencyConverter/FetchConverter.java
+++ project/CurrencyConverter/src/com/denesgarda/CurrencyConverter/FetchConverter.java
@@ -1,7 +1,16 @@
 package com.denesgarda.CurrencyConverter;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class FetchConverter extends Converter {
     public FetchConverter() {
         super();
+    }
+
+    public double convert(double before, Currency from, Currency to) throws IOException {
+        URLConnection connection = new URL("https://www.google.com/search?q=" + from.toString().toLowerCase() + "+to+" + to.toString().toLowerCase() + "+exchange+rate").openConnection();
+        return 0;
     }
 }
