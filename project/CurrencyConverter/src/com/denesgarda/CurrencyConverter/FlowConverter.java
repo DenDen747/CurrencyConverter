b86fce2a659df5debc5c0c9aefae6e6e30f49e13
DenDen747 <dsgarda@gmail.com>
2021-10-07 19:37:35 -0700

    Added Logic to FlowConverter

--- project/CurrencyConverter/src/com/denesgarda/CurrencyConverter/FlowConverter.java
+++ project/CurrencyConverter/src/com/denesgarda/CurrencyConverter/FlowConverter.java
@@ -1,11 +1,31 @@
 package com.denesgarda.CurrencyConverter;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class FlowConverter extends Converter {
     private int refreshRate;
+    private Currency[][] pattern;
+    private double[][] values;
+    private Timer timer;
+    private TimerTask refresher;
 
-    public FlowConverter(int refreshRate) {
+    public FlowConverter(Currency[][] pattern, int refreshRate) {
         super();
         this.refreshRate = refreshRate;
+        this.pattern = pattern;
+        for (Currency[] set : pattern) {
+            if (set.length != 2) {
+                throw new IndexOutOfBoundsException("Each set of the pattern must have exactly two values.");
+            }
+        }
+        values = new double[pattern.length][2];
+        refresher = new TimerTask() {
+            @Override
+            public void run() {
+                // Refresh all values
+            }
+        };
     }
 
     public int getRefreshRate() {
@@ -14,5 +34,19 @@ public class FlowConverter extends Converter {
 
     public void setRefreshRate(int refreshRate) {
         this.refreshRate = refreshRate;
+    }
+
+    public void start() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(refresher, 0, refreshRate);
+    }
+
+    public void stop() {
+        timer.cancel();
+        timer = null;
+    }
+
+    public void refresh() {
+        refresher.run();
     }
 }
