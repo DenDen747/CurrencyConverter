b81235ec1e71808337ab3e23bbf4efa9060519ec
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-10-08 08:59:46 -0700

    Refined FlowConverter

--- project/CurrencyConverter/src/com/denesgarda/CurrencyConverter/FlowConverter.java
+++ project/CurrencyConverter/src/com/denesgarda/CurrencyConverter/FlowConverter.java
@@ -1,31 +1,73 @@
 package com.denesgarda.CurrencyConverter;
 
+import java.io.IOException;
+import java.io.InvalidObjectException;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class FlowConverter extends Converter {
+    private boolean running;
+    private final FetchConverter base;
     private int refreshRate;
+    private boolean fetchOverride;
     private Currency[][] pattern;
-    private double[][] values;
+    private double[] values;
     private Timer timer;
-    private TimerTask refresher;
+    private final TimerTask refresher;
 
-    public FlowConverter(Currency[][] pattern, int refreshRate) {
+    public FlowConverter(Currency[][] pattern, int refreshRate, boolean fetchOverride) {
         super();
+        running = false;
+        base = new FetchConverter();
         this.refreshRate = refreshRate;
+        this.fetchOverride = fetchOverride;
         this.pattern = pattern;
         for (Currency[] set : pattern) {
             if (set.length != 2) {
                 throw new IndexOutOfBoundsException("Each set of the pattern must have exactly two values.");
             }
         }
-        values = new double[pattern.length][2];
+        values = new double[pattern.length];
         refresher = new TimerTask() {
             @Override
             public void run() {
-                // Refresh all values
+                for (int i = 0; i < pattern.length; i++) {
+                    try {
+                        double value = base.getExchangeRate(pattern[i][0], pattern[i][1]);
+                        values[i] = value;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         };
+    }
+
+    public double convert(double before, Currency from, Currency to) throws IOException {
+        if (running) {
+            boolean found = false;
+            double result = 0;
+            for (int i = 0; i < pattern.length; i++) {
+                if (pattern[i][0] == from && pattern[i][1] == to) {
+                    found = true;
+                    result = values[i];
+                }
+            }
+            if (!found) {
+                if (fetchOverride) {
+                    return base.convert(before, from, to);
+                } else {
+                    throw new InvalidObjectException("Unable to convert. The set wasn't provided in the arguments of the flow converter.");
+                }
+            }
+            return result;
+        } else {
+            throw new IllegalStateException("Converter is not running.");
+        }
+    }
+
+    public void allowFetchOverride(boolean fetchOverride) {
+        this.fetchOverride = fetchOverride;
     }
 
     public int getRefreshRate() {
@@ -37,11 +79,13 @@ public class FlowConverter extends Converter {
     }
 
     public void start() {
+        running = true;
         timer = new Timer();
         timer.scheduleAtFixedRate(refresher, 0, refreshRate);
     }
 
     public void stop() {
+        running = false;
         timer.cancel();
         timer = null;
     }
